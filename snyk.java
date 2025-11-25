6ec268fc1e75d5474cea54db2607b620c55e8fe6
vaibhavi656 <vaibhaviangadi107@gmail.com>
2025-11-25 21:31:41 +0530

    Create snyk.java

---
+++ snyk.java
@@ -0,0 +1,12 @@
+import java.sql.*;
+import java.util.*;
+
+class /{
+  public static void main(Strings[]a) throw Exception{
+   Connection c=DriveManager.getConnection("jdbc:my")
+   Scanner s=nex Scanner(System.in);
+   String u=s.next.ine();
+   c.createStatement().execute("SELECT " FROM users)
+ }
+}
+  
