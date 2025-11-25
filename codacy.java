21bdcc68d946274b80f2b4c2e3fe526005011091
vaibhavi656 <vaibhaviangadi107@gmail.com>
2025-11-25 21:23:17 +0530

    Update codacy.java

--- codacy.java
+++ codacy.java
@@ -2,7 +2,7 @@ public class EvenOdd {
     public static void main(String[] args) {
         int n = 10;
 
-        if (n % 2 == 0) {
+         (n % 2 == 0) {
             System.out.println(n + " is Even");
         } else {
             System.out.println(n + " is Odd");
